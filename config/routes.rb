8f1b45d711037cecbb3a5d9b9de8f892d9055b38
Newriver758 <shinkawa0109@gmail.com>
2024-11-24 23:58:38 +0900

    usersコントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
+  get 'calendars/index'
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: "calendars#index"
+  resources :users, only: [:edit, :update]
 end
