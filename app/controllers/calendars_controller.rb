a318d1f3187d147f559d6624c0b0dad0a9a2edd4
Newriver758 <shinkawa0109@gmail.com>
2024-12-01 09:07:50 +0900

    前の週・今日・次の週ボタンの実装

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,5 +1,8 @@
 class CalendarsController < ApplicationController
   def index
+    # パラメーターから日付を取得、なければ今日の日付をデフォルトに
+    @current_date = params[:start_date] ? Date.parse(params[:start_date]) : Date.today
+
     # 表示する期間を取得（例: 今週の月曜日から日曜日まで）
     @start_date = Date.today.beginning_of_week  # 週の始まり (月曜日)
     @end_date = Date.today.end_of_week          # 週の終わり (日曜日)
