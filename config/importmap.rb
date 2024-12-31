af3d4525070af0faad941bcfdc11861d5c66cf39
Newriver758 <shinkawa0109@gmail.com>
2025-01-01 01:51:37 +0900

    Javascriptの再設定

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,7 +4,4 @@ pin 'application'
 pin '@hotwired/turbo-rails', to: 'turbo.min.js'
 pin '@hotwired/stimulus', to: 'stimulus.min.js'
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js'
-pin_all_from 'app/javascript/controllers', under: 'controllers'
-pin '@hotwired/turbo', to: '@hotwired--turbo.js' # @8.0.12
-pin '@rails/actioncable/src', to: '@rails--actioncable--src.js' # @7.2.201
-pin 'application', preload: true
+pin_all_form 'app/javascript/controllers', under: 'controllers'
